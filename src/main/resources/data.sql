510fe14ae4e234d70773157946da4e6dcab1313d
Wael Rabadi <wrabadi@vmware.com>
2022-10-05 13:59:51 -0500

    Update db seed and use h2 console

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO reservation (id, truck_id, state) VALUES (1,1,0);
-INSERT INTO reservation (id, truck_id, state) VALUES (2,2,1);
+INSERT INTO reservation (truck_id, state) VALUES (1,0);
+INSERT INTO reservation (truck_id, state) VALUES (2,1);
+INSERT INTO reservation (truck_id, state) VALUES (3,0);
